83e5d60c0b3809b02ae7abbe3f83228bd1b4b859
corentinckf <c.chienkanfoon@gmail.com>
2021-03-10 16:05:01 +0100

    done

--- TP2/main.c
+++ TP2/main.c
@@ -13,38 +13,77 @@ int CNP(int n, int p)
     return retour;
 }
 
+int AckIter(int m, int n)
+{
+    pile_t * pile = initialiser_Pile(MAX);
+    element_t mi = m, *pmi = &mi;
+    element_t ni = n, *pni = &ni;
+    int etat = 0, * petat = &etat;
+    int fin = 0;
+    while (!fin)
+    {
+        if(mi)
+        {
+            while (ni)
+            {
+                empiler(pile,mi);
+                --ni;
+            }
+            --mi;
+            ni = 1;
+            
+        }
+        else
+        {
+            ++ni;
+            if(!estVide(pile))
+            {
+                depiler(pile, pmi, petat);
+                --mi;
+            }
+            else
+            {
+                fin = 1;
+            }
+        }
+    }
+    liberer_Pile(&pile);
+    return ni;
+    
+}
+
 
 
 int CNP_IT(int n, int p)
 {
     pile_t * pile = initialiser_Pile(MAX);
-    element_t ni = n, * pni = &ni;
-    element_t pi = p, * ppi = &pi;
-    int etat = 0, * petat = &etat, res = 0;
-    int fin = 0;
-
+    element_t pi = p, *ppi = &pi;
+    element_t ni = n, *pni = &ni;
+    int etat = 0, * petat = &etat;
+    int fin = 0,res = 0;
     while (!fin)
     {
-        while(ni == pi || !pi)
+        if(!(ni == pi || pi == 0))
         {
             empiler(pile,ni);
-            ++res;
+            empiler(pile,pi);
             --ni;
-        }
-        if(!estVide(pile))
-        {
-            depiler(pile,pni,petat);
-            depiler(pile,ppi,petat);
-            --ni;
-            --pi;
+            
         }
         else
         {
-            fin = 1;
+            ++res;
+            if(!estVide(pile))
+            {
+                depiler(pile, ppi, petat);
+                depiler(pile, pni, petat);
+                --ni;
+                --pi;
+            }else{
+                fin = 1;
+            }
         }
-        
     }
-
     liberer_Pile(&pile);
     return res;
     
@@ -54,11 +93,7 @@ int main()
 {
     printf("CNP(%d,%d) récursif = %d\n",5,3, CNP(5,3));
     printf("CNP(%d,%d) itératif = %d\n",5,3,CNP_IT(5,3));
-    pile_t * pile = initialiser_Pile(MAX);
-    element_t test = 2;
-    printf("%d\n", estVide(pile));
-    int oui = empiler(pile,test);
-    printf("%d\n", estVide(pile));
+    printf("Ackerman(2,1) : %d\n", AckIter(2,1));
 
     return 0;
 }
