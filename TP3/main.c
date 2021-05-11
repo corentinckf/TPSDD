0fb07329005717c9ee7d032f3aced1627c30c79a
pcchienkan <pcchienkan@ada.local.isima.fr>
2021-05-11 17:32:35 +0200

    correction construction dico/arbre

--- TP3/main.c
+++ TP3/main.c
@@ -2,26 +2,24 @@
 #include "arbre.h"
 #include "string.h"
 
-
-
 noeud_t * creer_Dico(char * string)
 {
     pile_t * pile = initialiser_Pile(MAX);
-    noeud_t * rac;
-    noeud_t ** prec=&rac; noeud_t* cour;
+    noeud_t * rac, *noeud;
+    noeud_t ** prec=&rac; noeud_t * cour = rac;
     int i;
-    int empiler_prochain = 0;
+    int empiler_prochain = 1;
     int etat = 0;
 
     int taille_string = (int) strlen(string);
 
-    //On parcourt toute la boucle => for
+    //On parcourt toute la string => for
     for(i=0; i < taille_string;++i)
     {
         switch(string[i])
         {
             case '*': 
-                prec = &(*prec)->fils;
+                prec = &cour->fils;
                 break;
             case '+':
                 depiler(pile,&cour,&etat);
@@ -34,17 +32,16 @@ noeud_t * creer_Dico(char * string)
             case ')': // On depile
                 depiler(pile,&cour,&etat);
                 break;
-            default: // Cas par default est en fait lettre quelconques
-            {
-                noeud_t * noeud = alloc_noeud(string[i],NULL,NULL);
+            default: // Cas par defaut est en fait lettre quelconques
+                noeud = alloc_noeud(string[i],NULL,NULL);
                 *prec = noeud;
+                cour = noeud;
                 if(empiler_prochain)
                 {
-                    empiler(pile,*prec);
+                    empiler(pile,cour);
                     empiler_prochain = 0;
                 }
-            }
-                
+                break;     
         }
     }
 
@@ -52,9 +49,17 @@ noeud_t * creer_Dico(char * string)
 }
 
 
+void affichage_Dico(noeud_t * racine)
+{
+    pile_t * pile = initialiser_Pile(MAX);
+    noeud_t ** prec = &racine, * cour = racine;
+
+
+}
 int main()
 {
-    char expression[] = "(a*(b*(a*T+i*(m*e)))+(R*(d*u)+(T*S))))";
+    //A externaliser sur un autre fichier
+    char expression[] = "(a*(a*(C+D)+b*E))";
 
     noeud_t * rac = creer_Dico(expression);
 
